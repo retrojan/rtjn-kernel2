0eff0df939966551300c0f1d679eb1e44d34cc9b
retrojan <175008938+retrojan@users.noreply.github.com>
2026-09-08 13:22:41 +0300

    Edited version

--- heads/version.h
+++ heads/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define RTJN_VERSION       "0.3.4"
+#define RTJN_VERSION       "0.5.14"
 #define RTJN_RELEASENAME   "retrojan"
 #define RTJN_AUTHOR        "retrojan"
 
